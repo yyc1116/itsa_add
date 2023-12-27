33bfe2c788e885eb27e09d3af8fbb619491b031f
yyc1116 <132194495+yyc1116@users.noreply.github.com>
2023-12-28 01:56:36 +0800

    Update mm_36.c

--- mm_36.c
+++ mm_36.c
@@ -6,7 +6,7 @@ int isPrime(int num){
     if(num <= 1){
         return 0;
     }
-    for (j = 2; j * j <= num; j++) {
+    for (j = 2; j * j <= num; j++) {    //因數 對稱, 做一半就行(找一半因數)
         if (num % j == 0) {
             return 0; // Not a prime number
         }
